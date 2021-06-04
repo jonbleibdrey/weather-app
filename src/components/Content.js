a2a94d426e1bfa710fad041f2e333d9f919a8624
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-04 11:21:47 -0400

    got api to work and getting data situated

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,27 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Content = () => {
-  const [weather, setWeather] = useState([]);
-  console.log(setWeather)
-
+  const [country, setCountry] = useState("");
+  const [weather, setWeather] = useState("");
+  
   useEffect(() => {
-    axios.get("")
-  }, [weather])
+    const apiKey = process.env.REACT_APP_API_KEY;
+
+    axios
+      .get(`http://api.openweathermap.org/data/2.5/weather?q=Texas&appid=${apiKey}`)
+      .then((res) => 
+      setCountry(res.data.name))
+      .catch((error) => console.log("we in error world", error));
+    }
+  , []);
 
   return (
     <div>
       <h1>
         weather goes here:
-        <p>{}</p>
       </h1>
+        {country}
     </div>
   );
 };
