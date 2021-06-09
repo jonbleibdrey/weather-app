845243e50051351d1f7ae8f43c999999b7a62875
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-09 15:34:42 -0400

    added table and some slight css

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,16 +1,31 @@
 import React from "react";
+import "../css/ContentList.css"
 
-const ContentList = ({weather}) => {
+const ContentList = ({ weather }) => {
   return (
-    <div>
-      {weather.name}
-      {weather.sys.country}
-      {weather.main.temp}
-      {weather.main.temp_min}
-      {weather.main.temp_max}
-      {weather.main.feels_like}
-      {weather.wind.speed}
-      {weather.weather[0].description}
+    <div className="contentList">
+      <table className="contentList-table" style={{width:"100%"}}>
+        <tr>
+          <th>State:</th>
+          <th>Country:</th>
+          <th>Tempature:</th>
+          <th>Feels like:</th>
+          <th>Min for the day:</th>
+          <th>Max for the day:</th>
+          <th>Speed:</th>
+          <th>Weather:</th>
+        </tr>
+        <tr>
+          <td>{weather.name}</td>
+          <td>{weather.sys.country}</td>
+          <td>{weather.main.temp}</td>
+          <td>{weather.main.feels_like}</td>
+          <td>{weather.main.temp_min}</td>
+          <td>{weather.main.temp_max}</td>
+          <td>{weather.wind.speed}</td>
+          <td>{weather.weather[0].description}</td>
+        </tr>
+      </table>
     </div>
   );
 };
