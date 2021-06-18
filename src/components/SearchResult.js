166335f0c69f929fa0408841c86035ec32856273
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-17 20:23:47 -0400

    edit text

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -5,8 +5,8 @@ const SearchResult = ({ searchResult }) => {
     <div>
       <h1>Your search results for {searchResult.name}</h1>
       <ul>
-        <li>current tempature is: {Math.round(searchResult.main.temp)}</li>
-        <li>weather: {searchResult.weather[0].description}</li>
+        <li>Current tempature is: {Math.round(searchResult.main.temp)}</li>
+        <li>Weather: {searchResult.weather[0].description}</li>
         <li>Feels Like: {Math.round(searchResult.main.feels_like)}</li>
         <li>Highs: {Math.round(searchResult.main.temp_max)}</li>
         <li>Lows: {Math.round(searchResult.main.temp_min)}</li>
