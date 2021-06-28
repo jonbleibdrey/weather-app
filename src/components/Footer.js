e5ff7b2491a3d96c232191635aecad00a533797a
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-28 11:34:16 -0400

    added css for footer for some alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,27 @@
 import React from "react";
+import "../css/Footer.css"
 import { AiFillFacebook } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillTwitterCircle } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>
-      <a href="https://www.facebook.com">
-        <AiFillFacebook />
-      </a>
-      <a href="https://www.twitter.com">
-        <AiFillTwitterCircle />
-      </a>
-      <a href="https://www.instagram.com">
-        <AiFillInstagram />
-      </a>
+    <div className="container">
+      <h1>
+        <a href="https://www.facebook.com">
+          <AiFillFacebook />
+        </a>
+      </h1>
+      <h1>
+        <a href="https://www.twitter.com">
+          <AiFillTwitterCircle />
+        </a>
+      </h1>
+      <h1>
+        <a href="https://www.instagram.com">
+          <AiFillInstagram />
+        </a>
+      </h1>
     </div>
   );
 };
