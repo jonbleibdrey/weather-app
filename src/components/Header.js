2c7faf5fc739d5c52ca2e174f6d7cd81be3e8477
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-29 14:34:22 -0400

    fixed up loadingand header screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import "../css/Header.css";
 const Header = () => {
-  return <div>❤️ LOOK UP ❤️</div>;
+  return (
+    <div className="header">
+      <img src="../logoRain.png" alt="rain" />
+      <h1 className="headerH1">❤️ LOOK UP ™ ❤️</h1>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
+import "../css/Loading.css"
 
 const Loading = ({ isLoading }) => {
   return (
@@ -7,7 +8,13 @@ const Loading = ({ isLoading }) => {
       {isLoading && (
         <div className="loading-spinner">
           <h1>
-            Loading... <Spinner animation="grow" variant="warning" size="lg" />{" "}
+            <Spinner animation="grow" variant="warning" size="lg" />
+            <Spinner animation="grow" variant="primary" size="lg" />
+            <Spinner animation="grow" variant="danger" size="lg" />
+            Loading 
+            <Spinner animation="grow" variant="warning" size="lg" />
+            <Spinner animation="grow" variant="primary" size="lg" />
+            <Spinner animation="grow" variant="danger" size="lg" />
           </h1>
         </div>
       )}
