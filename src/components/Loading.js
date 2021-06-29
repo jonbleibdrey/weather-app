91b74397874f579f904979082fafdc1e2bbf1aa0
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-29 14:12:57 -0400

    added loading spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Spinner from "react-bootstrap/Spinner";
 
-const Loading = ({isLoading}) => {
-    return (
-        <div>
-            {isLoading && <div>Loading...</div>}
+const Loading = ({ isLoading }) => {
+  return (
+    <div>
+      {isLoading && (
+        <div className="loading-spinner">
+          <h1>
+            Loading... <Spinner animation="grow" variant="warning" size="lg" />{" "}
+          </h1>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
